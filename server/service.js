71bb3ed2d448fe1617503d31c43c06ecdf0391d3
donkeykong91 <darylnator@gmail.com>
2018-07-28 01:13:33 -0700

    Added service for weather

---
+++ server/service.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+const express = require('express');
+
+const service = express();
+
+const request = require('superagent');
+
+const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
+
+
+service.get('/service/:location', (req, res, next) => {
+
+    
+    request.get('http://api.openweathermap.org/data/2.5/weather?q=' + 
+
+    req.params.location + `&APPID=${OPEN_WEATHER_API_KEY}&units=imperial`, 
+
+    (err, response) => {
+
+
+        if (err) {
+
+
+            console.log(err);
+
+            return res.sendStatus(404);
+
+
+        }
+
+        res.json({result: `${response.body.weather[0].description} at ${response.body.main.temp} degrees`});
+
+    });
+
+});
+
+module.exports = service;
